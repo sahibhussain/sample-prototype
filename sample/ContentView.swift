0688e100cb410dddccc0264910620f27f5cc601d
Sahib Hussain <sahib.hussain79@gmail.com>
2025-04-15 20:38:11 +0530

    Initial Commit

---
+++ sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sample
+//
+//  Created by Sahib Hussain on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Sahib Hussain on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
